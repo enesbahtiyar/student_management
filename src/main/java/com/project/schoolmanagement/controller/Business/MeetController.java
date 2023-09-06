0d3d0edf2b6532bd01fa7d136ef6f002e3b13c69
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2023-09-06 22:02:05 +0300

    getAllMeetByStudentAsList getAllMeetByAdvisorTeacherAsList endpoints

--- src/main/java/com/project/schoolmanagement/controller/Business/MeetController.java
+++ src/main/java/com/project/schoolmanagement/controller/Business/MeetController.java
@@ -5,14 +5,14 @@ import com.project.schoolmanagement.payload.response.business.MeetResponse;
 import com.project.schoolmanagement.payload.response.message.ResponseMessage;
 import com.project.schoolmanagement.service.business.MeetService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/meet")
@@ -27,5 +27,28 @@ public class MeetController
                                                   @RequestBody @Valid MeetRequest meetRequest)
     {
         return meetService.saveMeet(meetRequest, httpServletRequest);
+    }
+
+
+    @PutMapping("/update/{meetId}")
+    @PreAuthorize("hasAnyAuthority('TEACHER', 'ADMIN')")
+    public ResponseMessage<MeetResponse> updateMeet(@RequestBody @Valid MeetRequest meetRequest,
+                                                    @PathVariable Long meetId)
+    {
+        return meetService.updateMeet(meetRequest, meetId);
+    }
+
+    @GetMapping("/getAllMeetByAdvisorTeacherAsList")
+    @PreAuthorize("hasAnyAuthority('TEACHER')")
+    public ResponseEntity<List<MeetResponse>> getAllMeetByTeacher(HttpServletRequest httpServletRequest)
+    {
+        return ResponseEntity.ok(meetService.getAllMeetByAdvisorTeacherAsList(httpServletRequest));
+    }
+
+    @GetMapping("/getAllMeetByStudentAsList")
+    @PreAuthorize("hasAnyAuthority('STUDENT')")
+    public ResponseEntity<List<MeetResponse>> getAllMeetByStudent(HttpServletRequest httpServletRequest)
+    {
+        return ResponseEntity.ok(meetService.getAllMeetByStudentAsList(httpServletRequest));
     }
 }
--- src/main/java/com/project/schoolmanagement/service/business/MeetService.java
+++ src/main/java/com/project/schoolmanagement/service/business/MeetService.java
@@ -4,6 +4,7 @@ import com.project.schoolmanagement.entity.concretes.business.Meet;
 import com.project.schoolmanagement.entity.concretes.user.AdvisoryTeacher;
 import com.project.schoolmanagement.entity.concretes.user.Student;
 import com.project.schoolmanagement.exception.ConflictException;
+import com.project.schoolmanagement.exception.ResourceNotFoundException;
 import com.project.schoolmanagement.payload.mappers.business.MeetMapper;
 import com.project.schoolmanagement.payload.messages.ErrorMessages;
 import com.project.schoolmanagement.payload.messages.SuccessMessages;
@@ -17,12 +18,14 @@ import com.project.schoolmanagement.service.user.StudentService;
 import com.project.schoolmanagement.service.validator.DateTimeValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -84,5 +87,61 @@ public class MeetService
                 throw new ConflictException(ErrorMessages.MEET_HOURS_CONFLICT);
             }
         }
+    }
+
+    public ResponseMessage<MeetResponse> updateMeet(MeetRequest meetRequest, Long meetId)
+    {
+        Meet meet = isMeetExistById(meetId);
+        dateTimeValidator.checkTimeWithException(meetRequest.getStartTime(), meetRequest.getStopTime());
+        if(!(meet.getDate().equals(meetRequest.getDate()) &&
+                meet.getStartTime().equals(meetRequest.getStartTime()) &&
+                meet.getStopTime().equals(meetRequest.getStopTime())))
+        {
+            for (Long id : meetRequest.getStudentIds())
+            {
+                checkMeetConflict(id, meetRequest.getDate(), meetRequest.getStartTime(), meetRequest.getStopTime());
+            }
+        }
+
+        List<Student> students = studentService.getStudentByIdList(meetRequest.getStudentIds());
+        Meet updateMeet = meetMapper.mapMeetUpdateRequestToMeet(meetRequest, meetId);
+        updateMeet.setStudentList(students);
+        updateMeet.setAdvisoryTeacher(meet.getAdvisoryTeacher());
+
+        Meet savedMeet = meetRepository.save(updateMeet);
+
+        return ResponseMessage.<MeetResponse>builder()
+                .message(SuccessMessages.MEET_UPDATE)
+                .httpStatus(HttpStatus.OK)
+                .object(meetMapper.mapMeetToMeetResponse(savedMeet))
+                .build();
+    }
+
+    public Meet isMeetExistById(Long id)
+    {
+        return meetRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format(ErrorMessages.MEET_NOT_FOUND_MESSAGE, id)));
+    }
+
+    public List<MeetResponse> getAllMeetByAdvisorTeacherAsList(HttpServletRequest httpServletRequest)
+    {
+        String username = (String) httpServletRequest.getAttribute("username");
+        AdvisoryTeacher advisoryTeacher = advisorTeacherService.getAdvisorTeacherByUsername(username);
+
+        return meetRepository.getByAdvisoryTeacher_IdEquals(advisoryTeacher.getId())
+                .stream()
+                .map(meetMapper::mapMeetToMeetResponse)
+                .collect(Collectors.toList());
+    }
+
+    public List<MeetResponse> getAllMeetByStudentAsList(HttpServletRequest httpServletRequest)
+    {
+        String username = (String) httpServletRequest.getAttribute("username");
+        Student student = studentService.isStudentExistByUsername(username);
+
+        return meetRepository.findByStudentList_IdEquals(student.getId())
+                .stream()
+                .map(meetMapper::mapMeetToMeetResponse)
+                .collect(Collectors.toList());
     }
 }
